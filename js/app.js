dc3c82a521c5e2360804da46d039278fec1c9e3d
Divyansh Bhushan <divyansh@divyanshbhushan.xyz>
2023-08-18 17:44:29 +0530

    0.0.0.1

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
-console.clear()
+console.clear();
 // Go to FullScreen
 function toggleFullscreen() {
-    let navbar = document.querySelector('.navbar')
-    let fullScreenToggler = document.getElementById('fullScreenToggler')
+    let navbar = document.querySelector('.navbar');
+    let fullScreenToggler = document.getElementById('fullScreenToggler');
 
     if (fullScreenToggler.classList.contains('bg_fullscreen')) {
         if (window.document.body.requestFullscreen) {
@@ -12,34 +12,74 @@ function toggleFullscreen() {
         } else if (window.document.body.msRequestFullscreen) {
             window.document.body.msRequestFullscreen();
         }
-        fullScreenToggler.classList.replace('bg_fullscreen' , 'bg_fullscreen_exit')
-        fullScreenToggler.style.opacity = .2
-        navbar.style.display = "none"
-    }
-    else{
-        if(document.exitFullscreen) {
+        fullScreenToggler.classList.replace('bg_fullscreen', 'bg_fullscreen_exit');
+        fullScreenToggler.style.opacity = .2;
+        navbar.style.display = "none";
+    } else {
+        if (document.exitFullscreen) {
             document.exitFullscreen();
+        } else if (document.msexitFullscreen) {
+            document.msexitFullscreen();
+        } else if (document.mozexitFullscreen) {
+            document.mozexitFullscreen();
+        } else if (document.webkitexitFullscreen) {
+            document.webkitexitFullscreen();
         }
-            else if(document.msexitFullscreen) {
-                document.msexitFullscreen();
-            }
-            else if(document.mozexitFullscreen) {
-                document.mozexitFullscreen();
-            }
-            else if(document.webkitexitFullscreen) {
-                document.webkitexitFullscreen();
-            }
-            fullScreenToggler.classList.replace('bg_fullscreen_exit' , 'bg_fullscreen')
-            navbar.style.display = "flex"
-            fullScreenToggler.style.opacity = 1
+        fullScreenToggler.classList.replace('bg_fullscreen_exit', 'bg_fullscreen');
+        navbar.style.display = "flex";
+        fullScreenToggler.style.opacity = 1;
     }
-}
+};
 
-setInterval(function(){
+// Main Logic
+setInterval(function () {
+    // Time Variables
     let currentTime = new Date;
-    let seconds = currentTime.getSeconds()
-    let minutes = currentTime.getMinutes()
-    let hours = currentTime.getHours()
-    let date = currentTime.getDate()
-    let day = currentTime.getDay()
+    let currentSecond = currentTime.getSeconds();
+    let currentMinute = currentTime.getMinutes();
+    let currentHour = currentTime.getHours();
+    let currentDate = currentTime.getDate();
+    let currentDay = currentTime.getDay();
+    let currentMonth = currentTime.getMonth();
+    let currentYear = currentTime.getFullYear();
+
+    //DOM Variables
+    //Showing Time
+    let hours = document.querySelector('.hours');
+    let minutes = document.querySelector('.minutes');
+    let seconds = document.querySelector('.seconds');
+    let am_pm = document.querySelector('.am_pm');
+    let date = document.querySelector('.date');
+    let day = document.querySelector('.day');
+    let month = document.querySelector('.month');
+    let year = document.querySelector('.year');
+    //Customs
+    let clockType = document.getElementById('clockType');
+    let twelveHrClock = document.getElementById('12hrClock');
+    let twentryFourHrClock = document.getElementById('24hrClock');
+
+    twentryFourHrClock.addEventListener('click', () => {
+        clockType.innerHTML = "Clock Type: 24 Hour";
+        clockType.classList.replace('12hr', '24hr');
+    });
+
+    twelveHrClock.addEventListener('click', () => {
+        clockType.innerHTML = "Clock Type: 12 Hour";
+        clockType.classList.replace('24hr', '12hr');
+    });
+    
+    if (clockType.classList.contains('12hr')) {
+         if(currentHour == 12){
+            currentHour =  12
+        }
+        else if(currentHour > 12){
+            currentHour = currentHour - 12;
+        }
+        else{
+            currentHour
+        }
+        console.log(currentHour)
+    } 
+    else if(clockType.classList.contains('24hr')) {
+    }
 }, 1000);
